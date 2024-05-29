716cdc060cb42179077167ec6c747f0cb58f57c6
freshleesh <freshleesh@gmail.com>
2024-05-29 23:04:55 +0900

    planning 임시저장

--- trajectory_planning.py
+++ trajectory_planning.py
@@ -1,5 +1,6 @@
 import numpy as np
 import time
+import matplotlib.pyplot as plt
 
 def tp5(start, end, duration, step):
     # 시작값, 끝값, 작동시간, 궤적 개수
@@ -20,9 +21,44 @@ def tp5(start, end, duration, step):
     th = np.polyval(f, t)
     return th
 
-start = time.time()
+def draw_circle(center, radius, num_of_point):
+    # 중심 위치 [x, y], 반지름, 점 개수
+    
+    # 원을 이루는 각 0 ~ 2pi까지 생성
+    theta = np.linspace(0, 2*np.pi, num_of_point)
+
+    # theta를 매개변수로 하는 원의 궤도 return  [ [x, x, x], [y, y, y] ]
+    circle = np.array([center[0] + radius * np.cos(theta), center[1] + radius * np.sin(theta)])
+
+    return circle
+
+
+def draw_line(start, end, num_of_point):
+    #시작, 끝 점 좌표 [x,y], 점 개수
+
+    # start부터 end까지 linear하게 쪼개기
+    x = np.linspace(start[0], end[0], num_of_point) 
+    y = np.linspace(start[1], end[1], num_of_point) 
+
+    # return  [ [x, x, x], [y, y, y] ]
+    line = np.array([x, y])
+
+    return line
+
+def pickup_location(xx, aa):
+    # xx: deg, aa: cm
+
+    x = np.cos(np.deg2rad(xx)) * aa
+    y = np.sin(np.deg2rad(xx)) * aa
 th = tp5(0, 90, 10, 1000)
-print(time.time() - start)
+circle = draw_circle([0, 0], 1, 1000)
+line = draw_line([-1, -1], [1, 1], 1000)
+plt.plot(circle[0], circle[1], line[0], line[1])
+plt.show()
+
+
+
+
 
 
 
