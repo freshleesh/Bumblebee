f2a023af760150f159b74b43a36a2fc5dd43c7d9
freshleesh <freshleesh@gmail.com>
2024-05-31 02:46:09 +0900

    test용 파일

--- inverse.py
+++ inverse.py
@@ -2,6 +2,7 @@ from ikpy.chain import Chain
 from ikpy.link import OriginLink, URDFLink
 import numpy as np
 import matplotlib.pyplot as plt
+import trajectory_planning as tp
 
 
 #링크의 집합으로 된 chain 생성
@@ -35,34 +36,46 @@ arm_chain = Chain(name='left_arm', links=[
     ),
     URDFLink(
       name="link_4",
-      origin_translation=[0, 4, 0],
+      origin_translation=[0, 0, 5],
       origin_orientation=[0, 0, 0],
       rotation=[0, 0, 0],
     )
 ])
 
-#목표 위치와 방향
+# #목표 위치와 방향
 target_position = [26, 4, 11.3]
 target_orientation = [[1, 0, 0],
                       [0, 1, 0],
                       [0, 0, 1]] # 3x3 행렬
 
-#역기구학 계산
-inv = arm_chain.inverse_kinematics(target_position, target_orientation, orientation_mode='all')
+# #역기구학 계산
+angles = arm_chain.inverse_kinematics(target_position, target_orientation, orientation_mode='all')
 
-#디버깅을 위한 역산
-forw = arm_chain.forward_kinematics(inv)
+def move(angles):
+    pass
 
-print(target_position)
-print(forw)
+move(angles)
 
-#시각화
-ax = plt.figure().add_subplot(111, projection='3d')
-ax.set_xlabel('x')
-ax.set_xlim(0, 30)
-ax.set_ylabel('y')
-ax.set_ylim(-30, 30)
-ax.set_zlabel('z')
-ax.set_zlim(0, 30)
-arm_chain.plot(inv, ax)
-plt.show()
+# pickup = tp.pickup_location(45, 6)
+# destination = tp.place_location(35, 4)
+
+# way_points = []
+
+# for des in destination:
+#     target_position = pickup
+#     way_points.append(arm_chain.inverse_kinematics(target_position, target_orientation))
+#     target_position = des
+#     way_points.append(arm_chain.inverse_kinematics(target_position, target_orientation))
+
+
+
+# #시각화
+# ax = plt.figure().add_subplot(111, projection='3d')
+# ax.set_xlabel('x')
+# ax.set_xlim(0, 30)
+# ax.set_ylabel('y')
+# ax.set_ylim(-30, 30)
+# ax.set_zlabel('z')
+# ax.set_zlim(0, 30)
+# arm_chain.plot(inv, ax)
+# plt.show()
